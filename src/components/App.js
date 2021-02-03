b822ea429464c3730c8e1ac836dfdfac7818f52f
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-02-03 18:46:13 +0200

    fixed linters issues

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,11 @@ import React from 'react';
 import BookList from '../containers/booklist';
 import BookForm from '../containers/bookform';
 import Navbar from './navbar';
-import {useRef} from "react"
 
 function App() {
-
   return (
     <div className="app">
-      <div className="modall" ref={modall}>
+      <div className="modall">
         {}
       </div>
       <Navbar />
